5f54d8a608b105a00ba0f44260a21b3d070442fd
Alen Zeinolov <github@alenzeinolov.com>
2021-07-03 14:41:49 +0600

    Increase file limit to 20 MB

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const uploadFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   limits: {
-    fileSize: 10 * MB, // 10 megabytes
+    fileSize: 20 * MB, // 10 megabytes
   },
   fileFilter: uploadFilter,
 });
